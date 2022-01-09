ab95d9c84fefb1d563ced6200cbbb34387f94fa8
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-01-09 10:19:07 +0530

    port

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
-var port = 7000;
+
+const port = process.env.PORT || 6038;
+
 var cors = require('cors');
 app.use(cors());
 
-db;
-
-app.get('/', (req, res) => {
-    res.send("Welcome to the zomato server");
-    
-})
 
 
 const AuthController = require('./model/authcontroller');
