d057b938f69751dc71bd889d4ca549ecde0dd092
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-01-09 09:34:50 +0530

    login auth

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var db = require('./db');
+var port = 7000;
+var cors = require('cors');
+app.use(cors());
+
+const AuthController = require('./model/authcontroller');
+app.use('/api/auth',AuthController);
+
+app.listen(port,() => {
+    console.log('Listing to port '+port)
+})
