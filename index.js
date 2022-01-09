0cb0152a306e3918dc44847908b616263e3d3436
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-01-09 09:55:28 +0530

    mongodb server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
-var db = require('./db');
+const mongo = require('mongodb');
+const mongoclient = mongo.MongoClient;
 var port = 7000;
 var cors = require('cors');
 app.use(cors());
 
+var mongourl = 'mongodb+srv://sumanth:12345@sumanth.w8xsd.mongodb.net/zomato?retryWrites=true&w=majority';
+var db;
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Login api");
@@ -14,6 +17,10 @@ app.get('/', (req, res) => {
 const AuthController = require('./model/authcontroller');
 app.use('/api/model',AuthController);
 
-app.listen(port,() => {
-    console.log('Listing to port '+port)
+mongoclient.connect(mongourl, (err, client) => {
+    if(err) {console.log("error while connecting mongodb server");}
+    db = client.db('zomato');
+    app.listen(port, () => {
+        console.log(`listening on port no ${port}`)
+    })
 })
